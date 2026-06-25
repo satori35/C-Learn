e33a0ef2f461f7e5a76ff23a62ad33a4440d9421
satori35 <satori35@outlook.com>
2026-06-25 18:05:05 +0800

    Day1

---
+++ 1.第一个C语言程序/6_25 HelloWorld/6_25 HelloWorld/6_25.c
@@ -0,0 +1,8 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>//调用头文件
+
+int main()//main是程序的入口，一切函数的运行都从这里开始，main函数有且只有一个；int是整形数据类型，这里是为了成功结束后返回0.
+{
+	printf("HelloWorld\n");
+	return 0;
+}
---
+++ 2.数据类型/6_25/6_25/FileName.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main()
+{
+/*	printf("%d\n",sizeof(_Bool));//布尔型，只输出true和false（1/单位：字节）
+	printf("%d\n", sizeof(char));//字符型（1）
+	printf("%d\n", sizeof(short));//短整型（2）
+	printf("%d\n", sizeof(int));//整形（4）
+	printf("%d\n", sizeof(long));//长整型（4—8）
+	printf("%d\n", sizeof(long long));//更长的整形（8）
+	printf("%d\n", sizeof(float));//单精度浮点型（4）
+	printf("%d\n", sizeof(double));//双精度浮点型（8）
+	printf("%d\n", sizeof(long double));//长双精度浮点（8）
+	printf("%d\n", sizeof(long float));//长单精度浮点（8）
+	//这些数据类型可以将生活中的各种值分别存放不同的数据类型
+	return 0;*/
+
+	//使用：
+	char c = 'w';
+	int num = 2;
+	return 0;
+}
